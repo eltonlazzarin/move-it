a2f7d2768828f64fe84d958dc8abe1c14ea785ce
eltonlazzarin <elton.lazzarin@outlook.com>
2021-02-26 09:56:18 -0300

    remove provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,9 @@
 import React from 'react';
 
-import { ChallengesProvider } from '../contexts/ChallengesContext';
-
 import '../styles/Global.css';
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <ChallengesProvider>
-      <Component {...pageProps} />
-    </ChallengesProvider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
